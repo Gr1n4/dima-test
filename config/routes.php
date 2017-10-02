ea5a42ec99ce95ca1e33e16cbade0b1ccd0bfd6c
Alex <luchcko.a@gmail.com>
2017-10-02 19:00:20 +0300

    read me

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,6 @@
 <?php
 
 return array(
-  '^login$' => 'user/login',
-  '^register$' => 'user/register',
-  '^logout$' => 'user/logout',
-  '^upload$' => 'load/upload',
-  '^download/([a-zA-Z0-9.]+)' => 'load/download/$1',
-  '^delete/([a-zA-Z0-9.]+)' => 'load/delete_file/$1',
-  '^list$' => 'list/index',
   '^comment/new$' => 'comment/new',
   '^comment/add$' => 'comment/add',
   '^comment/remove/([a-zA-Z0-9.]+)$' => 'comment/remove_by_id/$1',
